7cc8b8df7e0875e021bfd4f1f56259afd80f7eee
sanze <hunter0122@163.com>
2018-10-24 21:09:07 +0800

    20181024更新

--- CBB_PF Maven Webapp/src/main/java/com/foo/manager/commonManager/thread/HttpHandleThread.java
+++ CBB_PF Maven Webapp/src/main/java/com/foo/manager/commonManager/thread/HttpHandleThread.java
@@ -1644,7 +1644,7 @@ public class HttpHandleThread implements Callable<Object> {
 				&& !orderDeclareHead.get("insuranceFee").toString().isEmpty()) {
 			colValues.add(orderDeclareHead.get("insuranceFee"));
 		} else {
-			colValues.add(null);
+			colValues.add(0);
 		}
 
 		colNames.add("WRAP_TYPE");
